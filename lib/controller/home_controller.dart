411cde4b860cf44836add73a52a952c5aeac79d6
Allef Sousa <allefsousa_1@hotmail.com>
2020-05-22 22:16:49 -0300

    Criando a store do app

---
+++ lib/controller/home_controller.dart
@@ -0,0 +1,52 @@
+import 'package:covidmobx/model/mundo.dart';
+import 'package:covidmobx/model/pais.dart';
+import 'package:covidmobx/repository/covid_api_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
+part 'home_controller.g.dart';
+
+
+class HomeController = _HomeControllerBase with _$HomeController;
+
+abstract class _HomeControllerBase with Store {
+  final api = Modular.get<CovidApiRepository>();
+
+  _HomeControllerBase() {
+    getInfoMundo();
+    getInfoPais();
+  }
+
+  @observable
+  int currentIndex = 0;
+
+  @observable
+  TextEditingController textFieldController = TextEditingController();
+
+  @observable
+  Mundo mundo;
+
+  @observable
+  Pais pais;
+
+  @action
+  getInfoMundo() async {
+    mundo = await api.getMundo();
+  }
+
+  @action
+  getInfoPais({String nomePais = 'brazil'}) async {
+    try {
+      pais = await api.getPais(pais: nomePais);
+    } catch (exception) {
+      return Container();
+    }
+  }
+
+  @action
+  changePage(int index) {
+    currentIndex = index;
+  }
+
+
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,18 +1,25 @@
+import 'package:covidmobx/controller/home_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends ModularState<HomePage, HomeController> {
+  final TextStyle whiteText = TextStyle(color: Colors.white);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        color: Colors.red,
-      ),
-    );
+    return Observer(builder: (_){
+      return Scaffold(
+        backgroundColor: Colors.white,
+        body: (this.controller.mundo != null &&this.controller.pais != null)
+        ? _buildBody()
+        :Center(child: CircularProgressIndicator(),),
+      );
+    });
   }
 }
